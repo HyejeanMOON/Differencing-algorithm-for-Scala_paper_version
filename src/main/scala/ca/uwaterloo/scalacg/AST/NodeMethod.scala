c015f9c742b3d8585a3b97ec412f082876634493
MOON <wenhaichen1992@gmail.com>
2018-10-19 23:31:25 +0900

    Update NodeMethod.scala

--- src/main/scala/ca/uwaterloo/scalacg/AST/NodeMethod.scala
+++ src/main/scala/ca/uwaterloo/scalacg/AST/NodeMethod.scala
@@ -1,26 +1,19 @@
 package ca.uwaterloo.scalacg.AST
 
 import ca.uwaterloo.scalacg.utils_for_cfg.{CFGFile, GlobalConstant}
-
 import scala.collection.mutable.Set
 
 
 trait NodeMethod extends GlobalConstant with CFGFile{
 
   var positionInNodeOfList:Int = 0
-
   var ID:Int = 0
-
   var nodeOfList = Array[Node]()
-
+  
   class Node(node:String) {
-
-
 
     val position = positionInNodeOfList
     positionInNodeOfList += 1
-
-
     val IDofNode = ID
     ID += 1
 
@@ -41,46 +34,32 @@ trait NodeMethod extends GlobalConstant with CFGFile{
 
     }
 
-
     final def getPosition:Int ={
-
       position
-
     }
 
-
     override def toString:String={
-
       node + "__" + getID
     }
 
     def getNodeString:String = {
-
       node
-
     }
 
 
     def getID:Int ={
-
       IDofNode
-
     }
-
 
     final def getLastNode:Node ={
-
       nodeOfList.take(position).last
-
     }
-
 
   }
 
   final def SearchInNodeOfList(str:String):Option[Node] ={
 
     val node = nodeOfList.find(_.getNodeString == str)
-
     node
 
   }
@@ -88,7 +67,6 @@ trait NodeMethod extends GlobalConstant with CFGFile{
   final def SearchInNodeOfListByID(ID:Int):Option[Node] ={
 
     val node = nodeOfList.find(_.getID == ID)
-
     node
 
   }
