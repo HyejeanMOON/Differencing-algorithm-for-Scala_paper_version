43672827cc653ce46e84adcdd8c4a2e9b6088090
MOON <wenhaichen1992@gmail.com>
2018-10-19 23:29:53 +0900

    Update HammockMethod.scala

--- src/main/scala/ca/uwaterloo/scalacg/AST/HammockMethod.scala
+++ src/main/scala/ca/uwaterloo/scalacg/AST/HammockMethod.scala
@@ -4,29 +4,22 @@ package ca.uwaterloo.scalacg.AST
 
 trait HammockMethod extends NodeMethod{
 
-
   var hammockID:Int = 0
 
   class Hammock extends Node(node = "Hammock"){
-
+    
     var hbID = hammockID
     hammockID += 1
-
     override def getID: Int = {
-
       hbID
-
     }
 
     override def getNodeString: String = {
-
       val NODE = "Hammock_" + hbID
-
       NODE
-
     }
-
+    
   }
-
+  
 }
 
